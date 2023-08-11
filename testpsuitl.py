6e537cb0cff6a2a7f13eeccf1684e47caad93a47
kdw <dongwooya123@gmail.com>
2023-08-11 12:28:07 +0900

    test python and node

--- testpsuitl.py
+++ testpsuitl.py
@@ -1,6 +1,25 @@
 import psutil
+import requests
+import time
+from dotenv import load_dotenv
+import os
+
 cpu_percent = psutil.cpu_percent()
 cpu_count = psutil.cpu_count()
 
 print(cpu_count)
 print(cpu_percent)
+
+load_dotenv(dotenv_path=".env.server_local")
+
+send_api = os.getenv("NODEJS_SERVER_URL")
+print(send_api)
+print("send_api: "+send_api)
+
+while True:
+    cpu_percent = psutil.cpu_percent(interval=0.5)
+    
+    # Node.js 서버로 데이터 전송
+    response = requests.post(send_api, json={"cpu_percent": cpu_percent})
+    
+    time.sleep(0.5)
